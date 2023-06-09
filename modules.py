186e05492ed573df54235914a3fe3c98e154fdbd
Oleg <email@example.com>
2023-06-09 14:40:57 +0300

    123

--- modules.py
+++ modules.py
@@ -1,24 +1,22 @@
 from pypdf import PdfMerger
+import os
 
+def Merge_files (list_pdfs):
+    merger = PdfMerger()
 
-n = int(input("Введите количество pdf файлов: "))
-pdfs = []
-for i in range(n):
-    print("Перенесите файл сюда и нажмите Enter: ", end = "")
-    pdfs.append(input())
+    for pdf in list_pdfs:
+        merger.append(pdf)
+
+    path = os.path.basename(list_pdfs[0]).replace(".pdf","")
+
+    merger.write("./files/"+path+"_merged.pdf")
+    merger.close()
     
-#pdfs = ['C:\\Users\\ponomarev\\Desktop\\uiop\\dev\\1.pdf', 'C:\\Users\\ponomarev\\Desktop\\uiop\\dev\\2.pdf']
-
-merger = PdfMerger()
-
-for pdf in pdfs:
-    merger.append(pdf)
-
-path = pdfs[0]
-path = '\\'.join(path.split('\\')[:-1])
-
-
-end_path = path + "\\res.pdf"
-print(end_path)
-merger.write(end_path)
-merger.close()
+    return ("./files/"+path+"_merged.pdf")
+    
+def Delete_file (list_path):
+    os.remove(list_path)
+        
+def Delete_many_files (list_path):
+    for path in list_path:
+        os.remove(path)
