8c24f8d86b7137a3f4c6d973e262af1fc3929678
Oleg <email@example.com>
2023-06-17 19:30:16 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import modules
 
+'''
 # --- Merge ---
+
+print ("--- Merge ---")
 n = int(input("Введите количество pdf файлов: "))
 pdfs = []
 for i in range(n):
@@ -15,5 +18,22 @@ input ()
 
 # удаление файла
 modules.Delete_file (result_path)
+'''
 
+
+# --- Split ---
+
+print ("--- Split ---")
+print("Перенесите файл сюда и нажмите Enter: ", end = "")
+pdf = input()
+ranges = input("Введите диапазоны страниц через пробел (Пример: 1-2 4-5): ").split()
+
+# файл появляется в папке files
+result_path = modules.Split_files(pdf, ranges)
+
+print ("файл создан: " + result_path)
+input ()
+
+# удаление файла
+modules.Delete_file (result_path)
 
